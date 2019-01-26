766e2dca3c0bab7c5f643de51671bbe4b45e6562
Robert Günzler <r@gnzler.io>
2019-01-26 03:38:38 +0100

    Use manifest-tool
    Travis' docker doesn't have the manifest subcommand

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-manifest() {
-    docker push $1-amd64
-    docker push $1-armv7hf
-    docker manifest create --amend $1 $1-amd64 $1-armv7hf
-    docker manifest annotate $1 $1-armv7hf --os linux --arch arm --variant armv7
-    docker manifest push $1
-}
+MANIFEST_TOOL_VERSION='v0.9.0'
+curl -sSfL https://github.com/estesp/manifest-tool/releases/download/$MANIFEST_TOOL_VERSION/manifest-tool-linux-amd64 \
+    -o manifest-tool
 
-MANIFEST_TOOL_VERSION='v1.0.0-rc'
+manifest_push() {
+    local version=$2
+    local ref=$1:$version
+
+    docker push $ref-amd64
+    docker push $ref-armv7hf
+
+    # can't use docker native manifest command
+    # docker manifest create --amend $1 $1-amd64 $1-armv7hf
+    # docker manifest annotate $1 $1-armv7hf --os linux --arch arm --variant v7
+    # docker manifest push $1
+
+    # process and push spec
+    sed -e "s|%%VERSION%%|$version|g" spec.template.yml > spec.yml
+    ./manifest-tool push from-spec ./spec.yml
+    rm spec.yml
+}
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 BASE=docker.io/$DOCKER_USERNAME/caddy
 # push versioned img
-manifest_push $BASE:$CADDY_VERSION
+manifest_push $BASE $CADDY_VERSION
 
 # tag/push latest img
 docker tag $BASE:$CADDY_VERSION-amd64 $BASE:latest-amd64
 docker tag $BASE:$CADDY_VERSION-armv7hf $BASE:latest-armv7hf
-manifest_push $BASE:latest
+manifest_push $BASE latest
