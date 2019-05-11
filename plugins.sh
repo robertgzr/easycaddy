4a04dbc4e67595a4e81025465998c3988c2d7533
Robert Günzler <r@gnzler.io>
2019-05-11 17:30:37 +0200

    Simplify plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -6,26 +6,28 @@ set -e
 if ! command -v goimport &>/dev/null; then
     echo "error: goimport not in PATH"
     echo ""
-
-    echo $PATH
-    ls -l $GOPATH/bin
     exit 1
 fi
 
-plugs="github.com/emersion/caddy-wkd"
-plugs="$plugs github.com/caddyserver/forwardproxy"
-plugs="$plugs github.com/caddyserver/dnsproviders/cloudflare"
-plugs="$plugs github.com/techknowlogick/caddy-s3browser"
-plugs="$plugs github.com/hacdias/caddy-minify"
-plugs="$plugs github.com/abiosoft/caddy-git"
-plugs="$plugs github.com/zikes/gopkg"
-plugs="$plugs github.com/captncraig/cors"
-plugs="$plugs github.com/nicolasazrak/caddy-cache"
-plugs="$plugs github.com/jung-kurt/caddy-cgi"
-# plugs="$plugs github.com/filebrowser/caddy"
+plugs='
+github.com/emersion/caddy-wkd
+github.com/caddyserver/forwardproxy
+github.com/caddyserver/dnsproviders/cloudflare
+github.com/techknowlogick/caddy-s3browser
+github.com/hacdias/caddy-minify
+github.com/abiosoft/caddy-git
+github.com/zikes/gopkg
+github.com/captncraig/cors
+github.com/nicolasazrak/caddy-cache
+github.com/epicagency/caddy-expires
+github.com/jung-kurt/caddy-cgi
+'
+# github.com/filebrowser/caddy
+
+flags=
+for p in $plugs; do
+    flags="$flags -add ${p}:_"
+done
 
 set -x
-
-for p in $plugs; do
-    goimport -w -get -add $p:_ run.go
-done
+goimport -w $flags run.go
